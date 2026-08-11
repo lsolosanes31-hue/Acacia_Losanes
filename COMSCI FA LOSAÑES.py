f3d8578e50bf78a57a5287eb847b7e74f9aa7e39
lsolosanes31-hue <lsolosanes31@src.pshs.edu.ph>
2026-08-11 09:32:10 +0800

    Add files via upload

---
+++ COMSCI FA LOSAÑES.py
@@ -0,0 +1,10 @@
+import math
+x1 = float(input("Enter x1:"))
+y1 = float(input("Enter y1:"))
+x2 = float(input("Enter x2:"))
+y2 = float(input("Enter y2:"))
+distance = math.sqrt((math.pow(x2-x1,2)) + (math.pow(y2-y1,2)))
+print("the distance between the two points is:" ,distance)
+ #Reflection
+ #Using the math library made my program easier to make because it already has sunctions like sqrt() and pow() , making it much simpler.
+ # Without these functions, I would have had to make longer and harder programs.
